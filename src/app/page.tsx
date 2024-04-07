cf15e7b94c04d74962250587d2248c5f96a582a5
Israel Bosun <israelbosun1@gmail.com>
2024-04-07 17:27:05 +0100

    first commital

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <h1 className="text-green-600">Hello world this is me and i am here and this is it</h1>
+    </main>
+  );
+}
